a954f563245be005b9a56464eaf03d1fb57c339b
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-08-25 13:59:57 -0400

    draft with starts of instance variables and functions on each page

--- battlefield.py
+++ battlefield.py
@@ -1,22 +1,49 @@
 from robot import Robot
 from dinosaur import Dinosaur
 
-
-# rugged environment with concrete and brick
-# urban, city setting
-# gravity 
-# sunny 
-# terrified, animated bystanders in the background
-# elements surrounding can be destroyed
-# can hurt either Robot or Dino
-
-
 class Battlefield:
     def __init__(self):
-        # rugged environment with concrete and brick
+        self.contains_robot=Robot()
+        self.contains_dino=Dinosaur()
+                
+        def run_game(self):
+            pass
+        def display_welcome():
+            print("Robot vs. Dinosaur in Future City")
+        
+        def display_winner():
+            if winner == Robot:
+                print (name, ', the Robot wins')
+            else: 
+                print (name, ', the Dinosaur wins')
+        
+        def battle_phase():
+            pass       
+        
+        def can_contain_robot():
+            pass
+        
+        def can_contain_dinosaur():
+            pass
+        def elements_can_be_destroyed():
+        pass
+
+        def can_hurt_Robot():
+            pass
+        
+        def can_hurt_Dino():
+            pass
+        
+# has_welcome_display
+# run_game
+# display_welcome
+# display_winner
+# battle_phase
+## EXTRAS below
+# rugged environment with concrete and brick
 # urban, city setting
-# gravity 
-# sunny 
+# gravity
+# sunny
 # terrified, animated bystanders in the background
 # elements surrounding can be destroyed
 # can hurt either Robot or Dino
--- main.py
+++ main.py
@@ -2,3 +2,19 @@ from battlefield import Battlefield
 
 # (5 points):  7 commits with good, descriptive messages. 
 # (10 points): battle conclude once either the Robot or the Dinosaur has its health points reduced to zero.
+
+ run_game()
+ display_welcome()
+ 
+ introduce_fighters()
+ introduce_fighters.robot.name()
+ 
+ introduce_fighters()
+ introduce_fighters.dinosaur.name()
+ 
+ battle_phase() #announce rules
+ 
+ announce_action() #what are we witnessing
+  
+ display_winner() #announce winner
+ 
